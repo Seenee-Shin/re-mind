b5808391a4c8f1ae5de96a00068e3ca4e9e007ad
RyuKwanHyun <sosto258@naver.com>
2022-01-27 17:17:33 +0900

    커밋

--- src/main/webapp/resources/js/common/common.js
+++ src/main/webapp/resources/js/common/common.js
@@ -4,10 +4,25 @@ const togle = document.querySelector('.headermenu_togle')
 const menu = document.querySelector('.dep1')
 const sub_menu = document.querySelectorAll('.dep2')
 
-togle.addEventListener('click', ()=>{
-    menu.classList.toggle('active')
-})
+// togle.addEventListener('click', ()=>{
+//     menu.classList.toggle('active')
+// })
 
-menu.addEventListener('click',()=>{
+// menu.addEventListener('click',()=>{
     
-})
+// })
+
+
+const closePopup = document.querySelectorAll(".close-popup-btn");
+const modal = document.querySelector(".modal");
+const layerPopupContent = document.querySelector(".layer-popup-content");
+// modal close + popup close
+for(let i =0; i< closePopup.length; i++) {
+    closePopup[i].addEventListener("click", () => {
+        // 모달 close
+        modal.style.display = "none";
+        // 팝업 내용 지우기
+        layerPopupContent.innerHTML = "";
+        
+    });
+}
