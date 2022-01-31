8070e6d7ef1536b95879650a0f0baa732fbd1b7b
RyuKwanHyun <sosto258@naver.com>
2022-02-01 07:07:13 +0900

    ㅂㅈㄷ

--- src/main/java/edu/kh/mind/member/controller/Aadgadg.java
+++ src/main/java/edu/kh/mind/member/controller/Aadgadg.java
@@ -9,11 +9,14 @@ import edu.kh.mind.member.model.vo.GoogleOAuthRequest;
 import edu.kh.mind.member.model.vo.GoogleOAuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
