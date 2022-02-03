986a805181656e4adf1a8c6d0fed501a95ff200a
Shin <heeyeon0705@gmail.com>
2022-02-03 21:20:00 +0900

    상담사 등록 controller

--- src/main/java/edu/kh/mind/adminPro/controller/AdminProController.java
+++ src/main/java/edu/kh/mind/adminPro/controller/AdminProController.java
@@ -2,6 +2,7 @@ package edu.kh.mind.adminPro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import edu.kh.mind.member.model.vo.Profession;
 @SessionAttributes({"loginPro"})
 public class AdminProController {
 	
+	//등록화면 연결
 	@Autowired
 	private AdminProService service;
 
@@ -24,14 +26,19 @@ public class AdminProController {
 		return "adminPro/proRegister";
 	}
 	
+	
+	//등록 기능구현
 	@RequestMapping(value = "proRegister", method = RequestMethod.POST )
-	public String proRegister(Profession profession) {
+	public String proRegister(Profession profession, Model model) {
 		
 		int result = service.proRegister(profession);
+		 if (result>0) {
+			 System.out.println("등록성공, 상세 정보를 등록해 주세요");
+	        } else {
+	        	System.out.println("등록 실패");
+	        }
 		
-		
-		
-		return null;
+		return "adminPro/proRegisterDetail";
 	}
 	
 	
