99305ee43a46b456edd3a5742e9c9c18b40c54de
ducky <ducky46@naver.com>
2022-02-04 15:42:37 +0900

    이메일 회원가입 메일 인증, 유효성 검사

--- src/main/java/edu/kh/mind/member/model/service/SignUpService.java
+++ src/main/java/edu/kh/mind/member/model/service/SignUpService.java
@@ -2,4 +2,16 @@ package edu.kh.mind.member.model.service;
 
 public interface SignUpService {
 
+	/** 아이디 중복 검사
+	 * @param inputId
+	 * @return result
+	 */
+	int idDupCheck(String inputId);
+
+	/** 닉네임 중복 검사
+	 * @param inputNickNm
+	 * @return result
+	 */
+	int nickNmDupCheck(String inputNickNm);
+
 }
