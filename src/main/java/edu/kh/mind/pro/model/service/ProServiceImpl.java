597f4b454529fe8b43834b98591737a5302ae4cd
dydwls <dydwls140@naver.com>
2022-02-04 22:29:26 +0900

    ㅇㄹ

--- src/main/java/edu/kh/mind/pro/model/service/ProServiceImpl.java
+++ src/main/java/edu/kh/mind/pro/model/service/ProServiceImpl.java
@@ -2,6 +2,7 @@ package edu.kh.mind.pro.model.service;
 
 import edu.kh.mind.member.model.vo.Profession;
 import edu.kh.mind.pro.model.dao.ProDAO;
+import edu.kh.mind.pro.model.vo.ReservationPayMent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,16 @@ public class ProServiceImpl implements ProService{
     @Override
     public List<Profession> selectAllProfession() {
         return dao.selectAllProfession();
+    }
+
+    // 총 금액 insert
+    @Override
+    public int priceInsert(int price, ReservationPayMent rv) {
+
+        // 상담예약결제 insert
+        int reservationPayNo = dao.reservationPaymentInsert(rv);
+
+
+        return 0;
     }
 }
