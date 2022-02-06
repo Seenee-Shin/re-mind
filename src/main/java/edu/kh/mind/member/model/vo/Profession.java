f12ab99bc3adbb13833d89604910ed08831f2bb8
DESKTOP-K5P3LA0\CHOYJ <dydwls140@naver.com>
2022-02-06 18:03:58 +0900

    커밋ㅁㅁㅁㅁㅁㅁ

--- src/main/java/edu/kh/mind/member/model/vo/Profession.java
+++ src/main/java/edu/kh/mind/member/model/vo/Profession.java
@@ -10,61 +10,19 @@ public class Profession {
     private String professionPhone;
     private Date professionEnrollDate;
     private String professionGender;
-    private String professionBirth;
-    private String professionAuthKey;
-    private int authStatus;
     private String remarks;
     private int statusCode;
-    private String worryName;
+    private String professionBirth;
+    private String professionAuthKey;
+    private String authStatus;
+    private String professionMajor;
+    private String professionIntro;
+    private String professionStory;
+    private String professionTag;
+    private String counselCategoryCode;
+    private String counselPrice;
 
     public Profession(){}
-
-    public void setWorryName(String worryName) {
-        this.worryName = worryName;
-    }
-
-    public String getWorryName() {
-        return worryName;
-    }
-
-
-
-    public int getProfessionNo() {
-        return professionNo;
-    }
-
-    public String getProfessionId() {
-        return professionId;
-    }
-
-    public String getProfessionPw() {
-        return professionPw;
-    }
-
-    public String getProfessionName() {
-        return professionName;
-    }
-
-    public String getProfessionPhone() {
-        return professionPhone;
-    }
-
-    public Date getProfessionEnrollDate() {
-        return professionEnrollDate;
-    }
-
-    public String getProfessionGender() {
-        return professionGender;
-    }
-
-
-    public String getRemarks() {
-        return remarks;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
 
     public void setProfessionNo(int professionNo) {
         this.professionNo = professionNo;
@@ -94,8 +52,6 @@ public class Profession {
         this.professionGender = professionGender;
     }
 
-
-
     public void setRemarks(String remarks) {
         this.remarks = remarks;
     }
@@ -104,46 +60,111 @@ public class Profession {
         this.statusCode = statusCode;
     }
 
-	public String getProfessionBirth() {
-		return professionBirth;
-	}
+    public void setProfessionBirth(String professionBirth) {
+        this.professionBirth = professionBirth;
+    }
 
-	public void setProfessionBirth(String professionBirth) {
-		this.professionBirth = professionBirth;
-	}
+    public void setProfessionAuthKey(String professionAuthKey) {
+        this.professionAuthKey = professionAuthKey;
+    }
 
-	public String getProfessionAuthKey() {
-		return professionAuthKey;
-	}
+    public void setAuthStatus(String authStatus) {
+        this.authStatus = authStatus;
+    }
 
-	public void setProfessionAuthKey(String professionAuthKey) {
-		this.professionAuthKey = professionAuthKey;
-	}
+    public void setProfessionMajor(String professionMajor) {
+        this.professionMajor = professionMajor;
+    }
 
-	public int getAuthStatus() {
-		return authStatus;
-	}
+    public void setProfessionIntro(String professionIntro) {
+        this.professionIntro = professionIntro;
+    }
 
-	public void setAuthStatus(int authstatus) {
-		this.authStatus = authstatus;
-	}
+    public void setProfessionStory(String professionStory) {
+        this.professionStory = professionStory;
+    }
 
-    @Override
-    public String toString() {
-        return "Profession{" +
-                "professionNo=" + professionNo +
-                ", professionId='" + professionId + '\'' +
-                ", professionPw='" + professionPw + '\'' +
-                ", professionName='" + professionName + '\'' +
-                ", professionPhone='" + professionPhone + '\'' +
-                ", professionEnrollDate=" + professionEnrollDate +
-                ", professionGender='" + professionGender + '\'' +
-                ", professionBirth='" + professionBirth + '\'' +
-                ", professionAuthKey='" + professionAuthKey + '\'' +
-                ", authStatus=" + authStatus +
-                ", remarks='" + remarks + '\'' +
-                ", statusCode=" + statusCode +
-                ", worryName='" + worryName + '\'' +
-                '}';
+    public void setProfessionTag(String professionTag) {
+        this.professionTag = professionTag;
+    }
+
+    public void setCounselCategoryCode(String counselCategoryCode) {
+        this.counselCategoryCode = counselCategoryCode;
+    }
+
+    public void setCounselPrice(String counselPrice) {
+        this.counselPrice = counselPrice;
+    }
+
+    public int getProfessionNo() {
+        return professionNo;
+    }
+
+    public String getProfessionId() {
+        return professionId;
+    }
+
+    public String getProfessionPw() {
+        return professionPw;
+    }
+
+    public String getProfessionName() {
+        return professionName;
+    }
+
+    public String getProfessionPhone() {
+        return professionPhone;
+    }
+
+    public Date getProfessionEnrollDate() {
+        return professionEnrollDate;
+    }
+
+    public String getProfessionGender() {
+        return professionGender;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getProfessionBirth() {
+        return professionBirth;
+    }
+
+    public String getProfessionAuthKey() {
+        return professionAuthKey;
+    }
+
+    public String getAuthStatus() {
+        return authStatus;
+    }
+
+    public String getProfessionMajor() {
+        return professionMajor;
+    }
+
+    public String getProfessionIntro() {
+        return professionIntro;
+    }
+
+    public String getProfessionStory() {
+        return professionStory;
+    }
+
+    public String getProfessionTag() {
+        return professionTag;
+    }
+
+    public String getCounselCategoryCode() {
+        return counselCategoryCode;
+    }
+
+    public String getCounselPrice() {
+        return counselPrice;
     }
 }
