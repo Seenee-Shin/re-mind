b4d8e081ba5ac06cbfb6256283458d626477952e
ducky <ducky46@naver.com>
2022-02-06 18:59:52 +0900

    mindPost insert

--- src/main/java/edu/kh/mind/board/model/service/MindPostService.java
+++ src/main/java/edu/kh/mind/board/model/service/MindPostService.java
@@ -2,6 +2,16 @@ package edu.kh.mind.board.model.service;
 
 import org.springframework.stereotype.Service;
 
+import edu.kh.mind.board.model.vo.MindPost;
+
 public interface MindPostService {
+
+	
+	
+	/** 포스트 등록 
+	 * @param post
+	 * @return
+	 */
+	int insertPost(MindPost post);
 
 }
