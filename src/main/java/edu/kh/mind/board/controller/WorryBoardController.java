e159adda72ef4f6f08872daf9b6173d7e8a96f33
stage <stage107@naver.com>
2022-02-09 02:11:17 +0900

    고민상담 글작성 수정중

--- src/main/java/edu/kh/mind/board/controller/WorryBoardController.java
+++ src/main/java/edu/kh/mind/board/controller/WorryBoardController.java
@@ -3,14 +3,14 @@ package edu.kh.mind.board.controller;
 import edu.kh.mind.board.model.service.WorryService;
 import edu.kh.mind.board.model.vo.Board;
 import edu.kh.mind.board.model.vo.WorryCategory;
+import edu.kh.mind.member.model.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,6 +47,30 @@ public class WorryBoardController {
         map.put("worryList", worryList);
 
         return map;
+    }
+
+    // 고민상담 글 작성
+    // 글작성 기능
+    @ResponseBody
+    @RequestMapping(value = "insert", method = RequestMethod.POST)
+    public int freeBoardInsert(Model model, @ModelAttribute("loginMember") Member loginMember,
+            @RequestPart(value="images", required=false) List<MultipartFile> images, HttpSession session,
+            Board board, String contentFiles) throws Exception {
+
+        board.setMemberNo(loginMember.getMemberNo());
+        //웹 접근경로(web path), 서버 저장경로(serverPath)
+        String webPath = "/resources/images/board/";
+
+        String serverPath= session.getServletContext().getRealPath(webPath);
+        //System.out.println(board);
+        System.out.println(webPath);
+        System.out.println(serverPath);
+        System.out.println(images);
+
+        //게시글 작성 후 상세 조회(DB에 입력된 게시글)할 boardNo
+//        int result = service.insertFreeBoard(board, images, webPath, serverPath);
+
+        return 0;
     }
 
 
