38ae3d22b162afafa3e28a19a8b635bd62e1868e
ducky <ducky46@naver.com>
2022-02-08 16:45:45 +0900

    털어놓기게시판, 댓글 생성

--- src/main/java/edu/kh/mind/board/model/service/SecretService.java
+++ src/main/java/edu/kh/mind/board/model/service/SecretService.java
@@ -2,6 +2,8 @@ package edu.kh.mind.board.model.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import edu.kh.mind.board.model.vo.Board;
 import edu.kh.mind.board.model.vo.WorryCategory;
 
@@ -10,6 +12,10 @@ public interface SecretService {
 	List<Board> selectSecretList();
 
 	List<WorryCategory> selectWorryCategory();
+
+	int updateBoard(Board board, List<MultipartFile> images, String webPath, String serverPath, String deleteImages);
+
+	Board selectBoard(int boardNo, int memberNo);
 	
 
 }
