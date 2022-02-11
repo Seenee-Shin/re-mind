a63781853744892d2772ad1ba9488f3a64d2d10c
ducky <ducky46@naver.com>
2022-02-12 05:15:38 +0900

    공지사항

--- src/main/java/edu/kh/mind/notice/model/service/NoticeService.java
+++ src/main/java/edu/kh/mind/notice/model/service/NoticeService.java
@@ -1,7 +1,15 @@
 package edu.kh.mind.notice.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import edu.kh.mind.notice.model.vo.Notice;
+
 public interface NoticeService {
+
+	List<Notice> noticeList();
+
+	Notice selectNotice(int noticeNo);
 
 }
--- src/main/java/edu/kh/mind/notice/model/service/NoticeServiceImpl.java
+++ src/main/java/edu/kh/mind/notice/model/service/NoticeServiceImpl.java
@@ -1,12 +1,27 @@
 package edu.kh.mind.notice.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import edu.kh.mind.notice.model.dao.NoticeDAO;
+import edu.kh.mind.notice.model.vo.Notice;
 
 @Service
 public class NoticeServiceImpl implements NoticeService {
 
 	@Autowired
 	private NoticeDAO dao;
+
+	@Override
+	public List<Notice> noticeList() {
+		return dao.noticeList();
+	}
+
+	@Override
+	public Notice selectNotice(int noticeNo) {
+		return dao.selectNotice(noticeNo);
+	}
+	
+	
 }
